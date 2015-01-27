44f4d5a8cd8f49069c4de1d20c8ab14819873693
fwittleder <franziska.wittleder@gmail.com>
2015-01-27 18:11:38 +0100

    navbar

---
+++ app/views/clients/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @client, :id, :name, :mail, :skype, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clients
+
   get 'pages/home'
 
   get 'pages/what'
