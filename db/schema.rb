b2a40bbf4eb03af3ae51a12ac1b7b1f727939335
fwittleder <franziska.wittleder@gmail.com>
2015-01-28 15:58:52 +0100

    gender

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127170127) do
+ActiveRecord::Schema.define(version: 20150128145255) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150127170127) do
     t.string   "skype"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gender"
   end
 
 end
